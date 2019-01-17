a48dd711c10a62fb601058fa232a7c90ee298dbc
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-17 21:48:52 +0530

    Add logger to script

--- jobs/goldeneagledirect/jobs.py
+++ jobs/goldeneagledirect/jobs.py
@@ -2,14 +2,30 @@ from redis import Redis
 from rq import Queue
 from crawler.crawl import getPage
 import sys
+from datetime import datetime
+import copy
+from lib.jobs_log import saveJob
+
 q = Queue(connection=Redis())
 start_url =  'https://www.goldeneagledirect.com/index.php?pg=%s&l=product_list&c=1'
 page_starts_at = 1
 page_ends_at = 16144
 
+jobs = {'is_crawled': 'False',
+        'is_parsed': 'False',
+        'storage_id': '',
+        'extension':'htm',
+        'insert_time': datetime.utcnow(),
+        'update_time': datetime.utcnow(), 
+        'crawl_queue': '', 
+        'parse_queue': '',
+        'job_script':'',
+        'parse_script':'', 
+        'priorities': 'high'}
+
 for i in range(page_starts_at, page_ends_at+1):
     page_url = start_url%(i)
-    result = getPage(page_url)
-    print(result)
-    #result = q.enqueue(getPage, page_url)
-    break
+    msg = copy.deepcopy(jobs)
+    msg['input'] = page_url
+    result = q.enqueue(getPage, page_url)
+    saveJob(msg)
