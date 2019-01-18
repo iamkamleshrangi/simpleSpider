d1b228d2fa06e2b0af8ddd024be809963a6ace22
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-18 12:10:19 +0530

    Logger of jobs

--- lib/jobs_log.py
+++ lib/jobs_log.py
@@ -2,8 +2,13 @@ from mongodb import operations
 from config_handler import handler
 
 obj = operations()
-def saveJob(msg):
-    spot_db = handler('database','spot_db')
-    spot_col = handler('database', 'spot_col')
-    status = obj.insert_one(spot_db, spot_col, msg)
+spot_db = handler('database','spot_db')
+spot_col = handler('database', 'spot_col')
+
+def saveJob(job):
+    status = obj.insert_one(spot_db, spot_col, job)
+    return status
+
+def updateJobStatus(job_id, job):
+    status = obj.update_to_mongo(spot_db, spot_col, {'job_id': job_id}, job)
     return status
