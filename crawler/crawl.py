343721b15d1ee8cd777e0334137806e1168046d5
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-18 17:00:57 +0530

    error logging into the system

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -6,29 +6,33 @@ from lib.jobs_log import checkFile, samejobCount
 
 PATH = handler('settings','public_path')
 def getPage(job):
-    url = job['input']
     job_id = job['job_id']
-    page = requests.get(url)
-    if page.status_code == 200:
-        html_content = page.content
-        storage_id = getId(html_content)
-        print(storage_id)
-        status, storage_path = checkFile(storage_id)
-        if status == False:
-            file_path = savePage(html_content)
-            job['storage_path'] = file_path
-        elif status == True:
-            job['storage_path'] = storage_path
+    try:
+        url = job['input']
+        page = requests.get(url)
+        if page.status_code == 200:
+            html_content = page.content
+            storage_id = getId(html_content)
+            print(storage_id)
+            status, storage_path = checkFile(storage_id)
+            if status == False:
+                file_path = savePage(html_content)
+                job['storage_path'] = file_path
+            elif status == True:
+                job['storage_path'] = storage_path
 
-        job['storage_id'] = storage_id
-        job['is_crawled'] = "True"
-        count = samejobCount(storage_id)
-        job['crawl_count'] = count
+            job['storage_id'] = storage_id
+            job['is_crawled'] = "True"
+            count = samejobCount(storage_id)
+            job['crawl_count'] = count
+            updateJobStatus(job_id, job)
+        else:
+            print('Already Crawled the page')
+            return True
+        return False
+    except Exception as e:
+        job['errors'] = e
         updateJobStatus(job_id, job)
-    else:
-        print('Already Crawled the page')
-        return True
-    return False
 
 def savePage(content):
     dir_path = PATH
