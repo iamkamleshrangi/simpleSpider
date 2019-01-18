e5e5b079cfa6e55b2361ede2bacc684dd57a9bae
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-18 12:10:05 +0530

    update job database

--- jobs/goldeneagledirect/jobs.py
+++ jobs/goldeneagledirect/jobs.py
@@ -1,10 +1,10 @@
 from redis import Redis
 from rq import Queue
-from crawler.crawl import getPage
 import sys
 from datetime import datetime
 import copy
 from lib.jobs_log import saveJob
+from lib.common import getId, getguId 
 
 q = Queue(connection=Redis())
 start_url =  'https://www.goldeneagledirect.com/index.php?pg=%s&l=product_list&c=1'
@@ -19,13 +19,16 @@ jobs = {'is_crawled': 'False',
         'update_time': datetime.utcnow(), 
         'crawl_queue': '', 
         'parse_queue': '',
-        'job_script':'',
-        'parse_script':'', 
-        'priorities': 'high'}
+        'job_script':'jobs.goldeneagledirect.jobs',
+        'crawl_script': 'crawler.crawl.getPage',
+        'parse_script':'parser.goldeneagledirect.parser', 
+        'priorities': 'high',
+        'storage_path': ''}
 
 for i in range(page_starts_at, page_ends_at+1):
     page_url = start_url%(i)
     msg = copy.deepcopy(jobs)
     msg['input'] = page_url
-    result = q.enqueue(getPage, page_url)
+    msg['job_id'] = getguId()
+    result = q.enqueue(msg['crawl_script'], msg)
     saveJob(msg)
