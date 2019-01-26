7f60d7619d21aa7443306f0894b956c01e8fd50e
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-01-26 18:50:33 +0530

    Add save file

--- lib/common.py
+++ lib/common.py
@@ -1,6 +1,8 @@
 import hashlib
 import uuid
+from lib.config_handler import handler
 
+  PATH = handler('settings','public_path')
 def getId(content):
    content_id = hashlib.md5(content.encode())
    return content_id.hexdigest()
@@ -8,3 +10,19 @@ def getId(content):
 def getguId():
     guid = uuid.uuid4().hex
     return guid
+
+def getContent(file_path):
+    handler = open(file_path, 'rb')
+    content = handler.read()
+    handler.close()
+    return content 
+
+def savePage(content):
+    dir_path = handler('settings','public_path')
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    file_name = uuid.uuid4().hex + '.htm'
+    file_path = dir_path + file_name
+    with open(file_path, 'wb') as f:
+        f.write(content)
+    return file_path
