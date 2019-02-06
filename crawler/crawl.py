4c2844fb9a6bcbc648da65d52a16e28a3f627228
Kamlesh Kumar Rangi <iamkamleshrangi@gmail.com>
2019-02-06 16:23:31 +0530

    added

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,8 +1,8 @@
-import requests
+import requests #ADD PROXIES 
 from lib.jobs_log import updateJobStatus, checkFile, samejobCount
 from lib.common import getId, savePage
 from lib.rq_queue import getConnections
-import datetime
+from datetime import datetime
 
 def getPage(job):
     job_id = job['job_id']
@@ -24,11 +24,22 @@ def getPage(job):
             job['update_time'] = datetime.utcnow()
             count = samejobCount(storage_id)
             job['crawl_count'] = count
-            result = q.enqueue(job['parse_script'], job)
-            updateJobStatus(job_id, job)
+            print(job)
+            #result = q.enqueue(job['parse_script'], job)
+            #updateJobStatus(job_id, job)
         else:
             print('Already Crawled the page')
     except Exception as e:
         print(e)
         job['errors'] = e
         updateJobStatus(job_id, job)
+
+job = {'is_crawled': 'False', 'is_parsed': 'False', 'storage_id': '', 'extension': 'htm',
+        'insert_time': '', 'update_time': '', 'crawl_queue': '', 'parse_queue': '',
+        'domain': 'cleaningparts', 'collection': 'cleaningparts_parts', 
+        'job_script': 'jobs.smchealth.jobs', 'crawl_script': 'crawler.crawl.getPage',
+        'parse_script': 'parser.cleaningparts.part.parser', 'priorities': 'high', 
+        'storage_path': '', 'crawl_count': 0, 
+        'input': {'url': 'http://cleaningparts.pl/szczotka-tt-ttb-345-p505', 'category': 'Wyprzedaż'},
+        'job_id': 'bc42e76c51a14d548cef0e3525867725'}
+getPage(job)
